57cae773861ab8ba8c4163eaa6723ba197795216
Anatoly <anatolyvoitov@gmail.com>
2025-02-05 20:53:49 +0200

    start

--- 30-homework/ViewController.swift
+++ 30-homework/ViewController.swift
@@ -1,9 +1,3 @@
-//
-//  ViewController.swift
-//  30-homework
-//
-//  Created by Anatoly Furman on 05/02/2025.
-//
 
 import UIKit
 
@@ -11,7 +5,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .brown
     }
 
 
