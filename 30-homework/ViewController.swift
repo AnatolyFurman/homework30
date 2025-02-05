7733f5cbab167142cbe7921f1bd79e4e937ed513
Anatoly <anatolyvoitov@gmail.com>
2025-02-05 20:48:29 +0200

    Initial Commit

---
+++ 30-homework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  30-homework
+//
+//  Created by Anatoly Furman on 05/02/2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
